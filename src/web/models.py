06d5b1e3e66c6636b6d60bcce8fae1fabd99f2c8
MariaFjodorowa <laks.salmon@gmail.com>
2018-12-12 22:09:47 +0300

    загрузка файлов в api из админки

--- src/web/models.py
+++ src/web/models.py
@@ -1,7 +1,41 @@
 from django.db import models
-
+from django.http import HttpResponseRedirect
+from web.views import handle_uploaded_file
 
 # Create your models here.
 class Record(models.Model):
     text = models.CharField(max_length=140)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+
+class MetaFile(models.Model):
+    """
+    Метаданные корпуса (файлы *.txt)
+    """
+    meta = models.FileField()
+
+    def save(self, request):
+        task_id = handle_uploaded_file(request.FILES['meta'])
+        return HttpResponseRedirect('main?task_id=' + task_id)
+
+
+class WordFile(models.Model):
+    """
+    Корпус с разметкой (файлы *.conll)
+    """
+    words = models.FileField()
+
+    def save(self, request):
+        task_id = handle_uploaded_file(request.FILES['words'])
+        return HttpResponseRedirect('main?task_id=' + task_id)
+
+
+class CollocationFile(models.Model):
+    """
+    Cписки коллокаций (файлы *.xlsx)
+    """
+    collocations = models.FileField()
+
+    def save(self, request):
+        task_id = handle_uploaded_file(request.FILES['collocations'])
+        return HttpResponseRedirect('main?task_id=' + task_id)

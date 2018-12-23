8724ba462c3db7dd753d5193e20c7afa8d7826f9
StanRed <32172485+StanRed@users.noreply.github.com>
2018-12-23 18:46:12 +0300

    new html, js and css added

--- src/web/static/main.js
+++ src/web/static/main.js
@@ -30,3 +30,17 @@ $(function () {
     get_status();
   }
 });
+
+$(document).ready(function() {
+    $('#collocationTable').DataTable();
+} );
+
+var http = require('http');
+var static = require('node-static');
+var file = new static.Server('.');
+
+http.createServer(function(req, res) {
+  file.serve(req, res);
+}).listen(8080);
+
+console.log('Server running on port 8080');

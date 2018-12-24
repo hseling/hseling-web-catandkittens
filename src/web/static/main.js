c9b921a3204ef571f8b1156dcb42395f6ac2a3ff
StanRed <32172485+StanRed@users.noreply.github.com>
2018-12-24 20:50:01 +0300

    main.js corrected, {% static %} links added

--- src/web/static/main.js
+++ src/web/static/main.js
@@ -27,20 +27,6 @@ $(function () {
       });
     };
 
-    get_status();
-  }
-});
-
 $(document).ready(function() {
     $('#collocationTable').DataTable();
 } );
-
-var http = require('http');
-var static = require('node-static');
-var file = new static.Server('.');
-
-http.createServer(function(req, res) {
-  file.serve(req, res);
-}).listen(8080);
-
-console.log('Server running on port 8080');
